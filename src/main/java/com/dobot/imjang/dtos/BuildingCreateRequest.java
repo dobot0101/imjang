800796aa71b927268ed9ac6fd82270ba41e3de10
Dohyun Lee <lhl890@naver.com>
2023-06-05 09:08:51 +0900

    [feature] BuildingService 유닛 테스트 코드 작성

--- src/main/java/com/dobot/imjang/dtos/BuildingCreateRequest.java
+++ src/main/java/com/dobot/imjang/dtos/BuildingCreateRequest.java
@@ -1,15 +1,8 @@
 package com.dobot.imjang.dtos;
 
-import java.util.List;
-import java.util.UUID;
-
 public class BuildingCreateRequest {
   private String name;
   private String address;
-  private String memo;
-  private Integer area;
-  private List<UUID> imageIds;
-  private List<UUID> informationItemIds;
 
   public String getName() {
     return name;
@@ -26,37 +19,4 @@ public class BuildingCreateRequest {
   public void setAddress(String address) {
     this.address = address;
   }
-
-  public String getMemo() {
-    return memo;
-  }
-
-  public void setMemo(String memo) {
-    this.memo = memo;
-  }
-
-  public Integer getArea() {
-    return area;
-  }
-
-  public void setArea(Integer area) {
-    this.area = area;
-  }
-
-  public List<UUID> getImageIds() {
-    return imageIds;
-  }
-
-  public void setImageIds(List<UUID> imageIds) {
-    this.imageIds = imageIds;
-  }
-
-  public List<UUID> getInformationItemIds() {
-    return informationItemIds;
-  }
-
-  public void setInformationItemIds(List<UUID> informationItemIds) {
-    this.informationItemIds = informationItemIds;
-  }
-
 }
---
+++ src/test/java/com/dobot/imjang/service/BuildingServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.dobot.imjang.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.dobot.imjang.dtos.BuildingCreateRequest;
+import com.dobot.imjang.entities.Building;
+
+@SpringBootTest
+@Transactional
+public class BuildingServiceImplTest {
+
+  @Autowired
+  BuildingServiceImpl buildingServiceImpl;
+
+  // @Transactional
+  // @Rollback
+  @Test
+  void 전체목록조회() {
+    List<Building> allBuildings = this.buildingServiceImpl.getAllBuildings();
+    boolean allElementsAreBuildings = allBuildings.stream().allMatch(Building.class::isInstance);
+    assertTrue(allElementsAreBuildings, "모든 요소가 Building 클래스의 인스턴트여야 합니다.");
+  }
+
+  @Test
+  void 저장() {
+    BuildingCreateRequest buildingCreateRequest = new BuildingCreateRequest();
+    buildingCreateRequest.setAddress("경인로 15길 70-22");
+    buildingCreateRequest.setName("테스트 빌딩");
+    Building building = buildingServiceImpl.createBuilding(buildingCreateRequest);
+
+    assertNotNull(building, "빌딩 객체가 생성되어야 합니다.");
+    assertEquals(buildingCreateRequest.getName(), building.getName(), "빌딩 이름이 일치해야 합니다.");
+    assertEquals(buildingCreateRequest.getAddress(), building.getAddress(), "빌딩 주소가 일치해야 합니다.");
+  }
+}
