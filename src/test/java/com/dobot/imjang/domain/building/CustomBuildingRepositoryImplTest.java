983c5e9e10fee4b409cd8cc7d61aa84156e43fc2
Dohyun Lee <lhl890@naver.com>
2024-03-13 15:30:05 +0900

    fix: CustomBuildingRepository 테스트 코드 수정

--- src/test/java/com/dobot/imjang/domain/building/CustomBuildingRepositoryImplTest.java
+++ src/test/java/com/dobot/imjang/domain/building/CustomBuildingRepositoryImplTest.java
@@ -1,11 +1,5 @@
 package com.dobot.imjang.domain.building;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,29 +7,35 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class CustomBuildingRepositoryImplTest {
 
-  @Autowired
-  private CustomBuildingRepository customBuildingRepository;
+    @Autowired
+    private BuildingRepository buildingRepository;
 
-  @Test
-  void findWithCursorPagination() {
-    UUID cursor = UUID.randomUUID();
-    LocalDateTime createdAt = LocalDateTime.now();
-    Pageable pageable = PageRequest.of(0, 10);
+    @Test
+    void findWithCursorPagination() {
+        UUID cursor = UUID.randomUUID();
+        LocalDateTime createdAt = LocalDateTime.now();
+        Pageable pageable = PageRequest.of(0, 10);
 
-    Slice<Building> result = customBuildingRepository.findWithCursorPagination(cursor, createdAt, pageable);
+        Slice<Building> result = buildingRepository.findWithCursorPagination(cursor, createdAt, pageable);
 
-    assertEquals(pageable.getPageSize(), result.getContent().size(), "The result size should match the page size");
-  }
+        assertEquals(pageable.getPageSize(), result.getContent().size(), "The result size should match the page size");
+    }
 
-  @Test
-  void findWithOffsetPagination() {
-    Pageable pageable = PageRequest.of(0, 10);
+    @Test
+    void findWithOffsetPagination() {
+        Pageable pageable = PageRequest.of(0, 10);
 
-    List<Building> result = customBuildingRepository.findWithOffsetPagination(pageable);
+        List<Building> result = buildingRepository.findWithOffsetPagination(pageable);
 
-    assertEquals(pageable.getPageSize(), result.size(), "The result size should match the page size");
-  }
+        assertEquals(pageable.getPageSize(), result.size(), "The result size should match the page size");
+    }
 }
